62ae37ee1c68b19d5b97fa6b47e7b2c864c1b986
fosslover <fosslover@duck.com>
2024-01-02 23:12:54 +0530

    feat: remove hide audio/video by id

--- src/components/radbot-widget/components/messages.tsx
+++ src/components/radbot-widget/components/messages.tsx
@@ -1,6 +1,5 @@
 import { MessageType } from "@/types/MessageType";
 import parse from "html-react-parser";
-import { AudioLines, Video } from "lucide-react";
 import { useState } from "react";
 
 export default function MessagesSection({
@@ -10,15 +9,6 @@ export default function MessagesSection({
 }) {
   const capitalize = (str: string) =>
     `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
-  const [audioId, setAudioId] = useState<number | null>(null);
-  const [videoId, setVideoId] = useState<number | null>(null);
-
-  const handleAudioId = (id: number) => {
-    setAudioId(audioId !== id ? id : null);
-  };
-
-  const handleVideoId = (id: number) => {
-    setVideoId(videoId !== id ? id : null);
   };
 
   return (
@@ -36,28 +26,14 @@ export default function MessagesSection({
             <div className="whitespace-pre-wrap">{parse(message.content)}</div>
             {message.role === "assistant" && (
               <>
-                {audioId === id && message.audio && (
-                  <audio controls className="my-2">
-                    <source src={message.audio} type="audio/mpeg"></source>
-                    Your browser does not support the audio element.
-                  </audio>
-                )}
-                {videoId === id && message.video && (
-                  <video width="320" height="240" controls className="my-2">
-                    <source src={message.video} type="video/mp4"></source>
-                    Your browser does not support the video element.
-                  </video>
-                )}
-                <div className="flex w-full justify-end items-center gap-1">
-                  <button onClick={() => handleAudioId(id)}>
-                    <AudioLines size={20} />
-                    <p className="sr-only">Toggle Audio</p>
-                  </button>
-                  <button onClick={() => handleVideoId(id)}>
-                    <Video size={24} />
-                    <p className="sr-only">Toggle Video</p>
-                  </button>
-                </div>
+                <audio controls className="my-2 w-full max-w-sm rounded-lg">
+                  <source src={message.audio} type="audio/mpeg"></source>
+                  Your browser does not support the audio element.
+                </audio>
+                <video controls className="my-2 rounded-lg w-full max-w-sm">
+                  <source src={message.video} type="video/mp4"></source>
+                  Your browser does not support the video element.
+                </video>
               </>
             )}
           </div>
